5956ce4d1265de43d79ff50e1a96c5da334e367c
Leandro Muller <hkl.muller@gmail.com>
2022-03-19 11:45:29 -0300

    Se agrego views

--- index/views.py
+++ index/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('<h1>Bienvenidos a mi proyecto de django</h1>')
