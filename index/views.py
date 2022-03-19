75fc5d34264b30b997bb80924f6e178415263520
Leandro Muller <hkl.muller@gmail.com>
2022-03-19 12:29:54 -0300

    Se borro todo

--- index/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def index(request):
-    return HttpResponse('<h1>Bienvenidos a mi proyecto de django</h1>')
